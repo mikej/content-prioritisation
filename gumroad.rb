9aa94bd647e71ea952c7bacc1eb81e2f64bf275c
Michael Josephson <mikej@users.noreply.github.com>
2022-08-06 14:51:31 +0100

    update/improve Gumroad script
    - extract JSON from saved HTML page
    - output both titles and creators in CSV format

--- gumroad.rb
+++ gumroad.rb
@@ -1,12 +1,25 @@
 require "json"
+require "nokogiri"
+require "csv"
 
-gumroad_json_string = File.read("gumroad.json")
+ARGV.each do |filename|
+  doc = Nokogiri::HTML(File.read(filename))
 
-json = JSON.parse(gumroad_json_string)
+  library_string = doc.at_css("script[data-component-name=\"LibraryPage\"]").content
 
-json["results"].each do |r| 
-  creator = r["product"]["creator"]
-  puts creator ? creator["name"] : ""
+  json = JSON.parse(library_string)
+
+  CSV($stdout) do |csv|
+    json["results"].each do |r|
+      name = r["product"]["name"]
+      title, subtitle = name.split(/:\s+/, 2).map(&:strip)
+      creator = r["product"]["creator"]
+      creator_name = creator ? creator["name"] : ""
+
+      # blank column is for manually filling in categorisation
+      csv << [title, creator_name, "", subtitle]
+    end
+  end
 end
 
 
