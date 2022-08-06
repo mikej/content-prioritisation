098a5c106b4fe9264a123b50d1078cc7970ce0ce
Michael Josephson <mikej@users.noreply.github.com>
2022-08-06 14:50:35 +0100

    update CSV column ordering to match Google Sheet

--- kindle.rb
+++ kindle.rb
@@ -3,15 +3,16 @@ require "csv"
 
 ARGV.each do |filename|
 	doc = Nokogiri::HTML(File.read(filename))
-	
+
 	title_nodes = doc.css(".digital_entity_title")
-	
+
   CSV($stdout) do |csv|
 	  title_nodes.each do |title_node|
       title, subtitle = title_node.text.split(/:\s+/, 2)
       author = title_node.next_sibling.text
-      
-      csv << [title, subtitle, author]
+
+      # blank column is for manually filling in categorisation
+      csv << [title, author, "", subtitle]
 	  end
   end
 end
