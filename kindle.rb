444ea1c6de6449b0a060984f3645d8bc0b0d8d6e
Michael Josephson <mikej@users.noreply.github.com>
2023-10-01 12:08:11 +0100

    fix indentation

--- kindle.rb
+++ kindle.rb
@@ -1,18 +1,20 @@
 require "nokogiri"
 require "csv"
+require "pry"
 
 ARGV.each do |filename|
-	doc = Nokogiri::HTML(File.read(filename))
+  doc = Nokogiri::HTML(File.read(filename))
 
-	title_nodes = doc.css(".digital_entity_title")
+  title_nodes = doc.css(".digital_entity_title")
 
   CSV($stdout) do |csv|
-	  title_nodes.each do |title_node|
-      title, subtitle = title_node.text.split(/:\s+/, 2)
+    title_nodes.each do |title_node|
+      title, subtitle = title_node.text.split(/:\s+/, 2).map(&:strip)
+      binding.pry
       author = title_node.next_sibling.text
 
       # blank column is for manually filling in categorisation
       csv << [title, author, "", subtitle]
-	  end
+    end
   end
 end
