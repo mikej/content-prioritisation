f9a27823abc550da49f1c5be769c16a0ac825bc7
Michael Josephson <mikej@users.noreply.github.com>
2023-10-01 16:22:33 +0100

    add acquired on date to Kindle CSV

--- kindle.rb
+++ kindle.rb
@@ -14,15 +14,21 @@ ARGV.each do |filename|
   CSV($stdout) do |csv|
     title_nodes.each do |title_node|
       title, subtitle = title_node.text.split(/:\s+/, 2).map { |s| clean(s) }
-      author = clean(title_node.next_element.text)
+
+      author_node = title_node.next_element
+      author = clean(author_node.text)
 
       # title_node will have an id like "content-title-ABCD2134" so we can extract the ASIN from this and use it
       # to build a product page URL
       asin = title_node["id"].match(/^content-title-(.+)$/).captures[0]
       link = "https://www.amazon.co.uk/gp/product/#{asin}"
 
+      date_acquired_node = doc.css("#content-acquired-date-#{asin} text()")[1]
+      date_string = date_acquired_node.text.strip
+      acquired_on = Date.strptime(date_string, "%d %B %Y")
+
       # blank column is for manually filling in categorisation
-      csv << [title, author, "", subtitle, link]
+      csv << [title, author, nil, subtitle, link, acquired_on]
     end
   end
 end
